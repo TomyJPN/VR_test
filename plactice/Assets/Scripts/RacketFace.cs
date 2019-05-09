9ecf3dd6e2a251dbaeab2ea18f379df49d97a29e
TomyJPN <masaya120331@gmail.com>
2019-05-10 00:59:50 +0900

    ラケットの物理演算実装

---
+++ plactice/Assets/Scripts/RacketFace.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//参考：https://qiita.com/ashtkn/items/16c0adcbf1ba15dcc7c9
+public class RacketFace : MonoBehaviour
+{
+  [SerializeField]
+  GameObject faceFollowerPrefab;
+
+  private GameObject faceFollower;
+
+  void OnEnable() {
+    faceFollower = Instantiate(faceFollowerPrefab, transform.position, transform.rotation);
+    faceFollower.GetComponent<RacketFaceFollower>().SetTarget(gameObject);
+  }
+
+  void OnDisable() {
+    Destroy(faceFollower);
+  }
+}
---
+++ plactice/Assets/Scripts/RacketFaceFollower.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//参考：https://qiita.com/ashtkn/items/16c0adcbf1ba15dcc7c9
+public class RacketFaceFollower : MonoBehaviour
+{
+  [SerializeField]
+  float sensitivity = 50f;
+
+  private Rigidbody rb;
+  private GameObject target;
+  private Vector3 velocity;
+
+  void Awake() {
+    rb = GetComponent<Rigidbody>();
+  }
+
+  public void SetTarget(GameObject target) {
+    this.target = target;
+  }
+
+  void FixedUpdate() {
+    if (target != null) {
+      velocity = (target.transform.position - rb.transform.position) * sensitivity;
+      rb.velocity = velocity;
+      rb.transform.rotation = transform.rotation;
+      transform.rotation = target.transform.rotation;
+    }
+  }
+}
