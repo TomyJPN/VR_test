2ca59db28cc0e408c116ad714523f12ce0d867a2
TomyJPN <masaya120331@gmail.com>
2019-05-09 14:06:14 +0900

    ピンポン玉の発射台を設置

---
+++ plactice/Assets/Scripts/HassyaDai.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HassyaDai : MonoBehaviour {
+  public GameObject ball;
+  Rigidbody rb;
+
+  float x = 0;
+  float y = 0;
+  float z = 0;
+
+  // Start is called before the first frame update
+  void Start() {
+    Invoke("hassya", 3f);
+  }
+
+  // Update is called once per frame
+  void Update() {
+
+  }
+
+  void hassya() {
+    GameObject ball_ = Instantiate(ball, this.gameObject.transform.position, transform.rotation);
+    rb = ball_.GetComponent<Rigidbody>();
+    x = Random.Range(13f, 16f);
+    z = Random.Range(-1f, 1f);
+    Debug.Log(x + "," + z);
+    Vector3 force = new Vector3(x * 0.01f, y * 0.01f, z * 0.01f);  // 力を設定
+    rb.AddForce(force, ForceMode.Impulse);
+    Invoke("hassya", 3f);
+  }
+}
